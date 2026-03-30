e91e3e25c0e092a46229b04ff576f0962408e772
ayaborsali <borsaliaya1@gmail.com>
2026-03-30 22:00:54 +0200

    fix export pool

--- rh-backend/config/db.js
+++ rh-backend/config/db.js
@@ -5,13 +5,13 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   ssl: {
-    rejectUnauthorized: false, // 🔥 IMPORTANT pour Render
+    rejectUnauthorized: false,
   },
 });
